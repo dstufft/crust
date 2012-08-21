340bf1fb8d35e733bde43391e8125e7e26e0ddba
Donald Stufft <donald.stufft@gmail.com>
2012-08-21 08:02:33 -0400

    Add a hook for vendoring requests

---
+++ crust/requests.py
@@ -0,0 +1,4 @@
+# This file acts as a simple hook for vendoring requests.
+# Simply delete crust/requests.py and replace it with the requests directory from
+#     the requests package.
+from requests import *
