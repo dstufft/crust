e2bbd8b26e2bac4699105146b8a5c1d4617b117e
Donald Stufft <donald.stufft@gmail.com>
2012-08-22 17:06:50 -0400

    Make api a decorator for classes that register and bind it

--- crust/api.py
+++ crust/api.py
@@ -1,3 +1,5 @@
+import copy
+
 from . import requests
 
 
@@ -11,3 +13,18 @@ class Api(object):
 
         self.url = url
         self.session = session
+        self.resources = {}
+
+    def __call__(self, resource):
+        cls = copy.deepcopy(resource)
+        cls._meta.api = self
+
+        self.resources[resource._meta.resource_name] = cls
+
+        return cls
+
+    def __getattr__(self, name):
+        if name in self.resources:
+            return self.resources[name]
+
+        raise AttributeError("'{0}' object has no attribute '{1}'".format(self.__class__.__name__, name))
