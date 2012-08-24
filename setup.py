a2b91dbfb5a1223db633d2a4fd40ce0fa77064db
Donald Stufft <donald.stufft@gmail.com>
2012-08-24 17:44:16 -0400

    bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="crust",
-    version="0.1",
+    version="0.2",
 
     description="Framework for Tastypie API Clients",
     long_description=open("README.rst").read(),
